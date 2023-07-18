b55f45ab363447c20990999a7476793ebe7d7c07
Vivek Patel <vpatel360@gmail.com>
2023-07-18 09:55:58 -0400

    commit tree 25aced0d2efc07c2fd8e3343fe105d95f5e046c0

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
-
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+
+  create isPalindrome function 
+  - set two variables at opposite ends of string
+    - increment/decrement and test for equality
+  -
+
+
 */
 
+const isPalindrome = function (str) {
+  
+  let j = str.length
+  for (let i = 0; i < str.length/2; i++) {
+    j--
+    if (str.charAt(i) !== str.charAt(j)) return false;
+  }
+  return true;
+};
+
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,9 +30,7 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
